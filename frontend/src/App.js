0ee10b611699a9b2abc07779104bf08cb36d6926
rajkumarsahu89 <sahukumarraj89@gmail.com>
2023-02-23 23:58:42 +0530

    signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+
 import { AdminPage } from './Pages/AdminPage';
 
 function App() {
   return (
     <div className="App">
-    
+    <Signup/>
+    {/* <Login/> */}
     </div>
   );
 }
