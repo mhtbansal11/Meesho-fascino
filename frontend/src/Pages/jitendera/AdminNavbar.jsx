ca3440cabe04c03cf7da0adbd8949c4088e6a1fb
jitendra4545 <jitendra45.js45@gmail.com>
2023-02-26 16:07:06 +0530

    added

--- frontend/src/Pages/jitendera/AdminNavbar.jsx
+++ frontend/src/Pages/jitendera/AdminNavbar.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import { Grid, GridItem } from '@chakra-ui/react'
-import { Sidebar } from './AdminSidebar'
+import React, { useState } from 'react'
+import { Grid, GridItem, Link, Button, Box } from '@chakra-ui/react'
+
 
 import { AllRoutes } from '../../routes/AllRoutes'
 import { AdminTop } from './AdminTop'
+import { AiOutlineHome } from 'react-icons/ai'
+import { GoDiffAdded } from 'react-icons/go'
+import { MdProductionQuantityLimits } from 'react-icons/md'
+import { SlPeople } from 'react-icons/sl'
+import { RiUserSettingsLine, RiLogoutBoxLine } from 'react-icons/ri'
+import { CiLogout } from 'react-icons/ci'
+import { AdminDashboard } from './AdminDashboard'
+import { AdminAddProduct } from './AdminAddProduct'
+import { Products } from './AdminProducts'
+import { AdminUserData } from './AdminUserData'
 export const AdminNavbar = () => {
+
+  const [dashboard, setdashboard] = useState(true)
+  const [addProduct, setaddProduct] = useState(false)
+  const [product, setproduct] = useState(false)
+  const [customer, setcustomer] = useState(false)
+  const handleDashboard = () => {
+    setaddProduct(false)
+    setdashboard(true)
+    setproduct(false)
+    setcustomer(false)
+  }
+  const handleaddProduct = () => {
+    setaddProduct(true)
+    setdashboard(false)
+    setproduct(false)
+    setcustomer(false)
+  }
+
+
+  const handleProduct = () => {
+    setaddProduct(false)
+    setdashboard(false)
+    setproduct(true)
+    setcustomer(false)
+  }
+
+  const handleCustomer = () => {
+    setaddProduct(false)
+    setdashboard(false)
+    setproduct(false)
+    setcustomer(true)
+  }
+
   return (
 
     <Grid
@@ -19,14 +62,24 @@ export const AdminNavbar = () => {
       fontWeight='bold'
     >
 
-      <GridItem boxShadow={' rgba(0, 0, 0, 0.24) 0px 3px 8px;'} zIndex={'999'} width='100%' position={'fixed'} top='0' pl='2' bg='white' area={'header'}>
+      <GridItem boxShadow={' rgba(0, 0, 0, 0.24) 0px 3px 8px;'}  zIndex={'999'} width='100%' position={'fixed'} top='0' pl='2' bg='white' area={'header'}>
         <AdminTop />
       </GridItem>
-      <GridItem  position={'fixed'} paddingRight='10px' boxShadow=' rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;' bg='white' height={'100vh'} top={'70px'} zIndex='1' pl='2' area={'nav'}>
-        <Sidebar />
+      <GridItem position={'fixed'} paddingRight='10px' boxShadow=' rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;' bg='white' height={'100vh'} top={'70px'} zIndex='1' pl='2' area={'nav'}>
+        <Box bg='white' position={'sticky'} display={'grid'} mt='20px' gap={'2'}>
+          <Button onClick={handleDashboard} bg={'white'} _focus={{ bg: 'blue', color: 'white' }} _hover={{ bg: 'blue.200', color: 'white' }} p={'30px 0'} w='220px' fontSize={'17px'} fontWeight={'bold'}  ><AiOutlineHome />  DASHBOARD</Button>
+          <Button onClick={handleaddProduct} bg={'white'} _focus={{ bg: 'blue', color: 'white' }} _hover={{ bg: 'blue.200', color: 'white' }} p={'30px 0'} w='220px' fontSize={'17px'} fontWeight={'bold'}  > <GoDiffAdded />  ADD PRODUCT</Button>
+          <Button onClick={handleProduct} bg={'white'} _focus={{ bg: 'blue', color: 'white' }} _hover={{ bg: 'blue.200', color: 'white' }} p={'30px 0'} w='220px' fontSize={'17px'} fontWeight={'bold'}  ><MdProductionQuantityLimits />  PRODUCTS</Button>
+          <Button onClick={handleCustomer} bg={'white'} _focus={{ bg: 'blue', color: 'white' }} _hover={{ bg: 'blue.200', color: 'white' }} p={'30px 0'} w='220px' fontSize={'17px'} fontWeight={'bold'}  ><SlPeople />  CUSTOMERS</Button>
+          <Button bg={'white'} _focus={{ bg: 'blue', color: 'white' }} _hover={{ bg: 'blue.200', color: 'white' }} p={'30px 0'} w='220px' fontSize={'17px'} fontWeight={'bold'}  > <RiUserSettingsLine />   SETTING</Button>
+          <Button bg={'white'} mt='150px' _hover={{ color: 'white', bg: 'red' }} p={'30px 0'} w='220px' fontSize={'17px'} fontWeight={'bold'}  > <RiLogoutBoxLine /> __ LOGOUT</Button>
+        </Box>
       </GridItem>
       <GridItem pl='2' bg='' area={'main'}>
-        {/* <AllRoutes /> */}
+        {dashboard ? <AdminDashboard /> : ""}
+        {addProduct ? <AdminAddProduct /> : ""}
+        {product ? <Products /> : ""}
+        {customer ? <AdminUserData /> : ""}
       </GridItem>
     </Grid>
   )
